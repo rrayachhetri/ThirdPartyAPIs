3bd48e5e35d6932e86c29e4b5164b664c63725b5
rrayachhetri <rrayachhetri@gmail.com>
2021-09-07 18:39:24 -0700

    colored textarea

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,3 @@
-
-
 var currentDate = moment().format("MMMM Do YYYY, h:mm a");
 $("#currentDay").text(currentDate);
 
@@ -17,11 +15,12 @@ $(document).ready(function () {
 
 
     function tracKtime() {
+
         var currentHour = moment().hour();
-
+        console.log(currentHour);
         $(".time-block").each(function () {
-            var timeBlock = parseInt($(this).atrr("id").split("hour")[1]);
-
+            var timeBlock = parseInt($(this).attr("id"));
+            console.log(timeBlock);
             if (timeBlock < currentHour) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
@@ -42,5 +41,15 @@ $(document).ready(function () {
         })
     }
 
-  tracKtime();
-})
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+
+    tracKtime();
+});
